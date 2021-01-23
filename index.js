a33a4d2e024be5b893173bbaff963fd4d75cb7d2
Yuri Zakharov <yuri.zaharov@gmail.com>
2021-01-23 19:51:44 +0300

    Change mongoose connect options

--- index.js
+++ index.js
@@ -26,7 +26,20 @@ console.log("Mobile context of this instance is:", mobileContext);
 const smsAllowInterval = process.env.SMS_ALLOW_INTERVAL;
 console.log("Interval between sending messages is set to:", smsAllowInterval, "min");
 
-mongoose.connect(mongoUri);
+// Setting mongoose parameters
+const options = {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    autoIndex: false, // Don't build indexes
+    poolSize: 10, // Maintain up to 10 socket connections
+    serverSelectionTimeoutMS: 5000, // Keep trying to send operations for 5 seconds
+    socketTimeoutMS: 45000, // Close sockets after 45 seconds of inactivity
+    family: 4 // Use IPv4, skip trying IPv6
+};
+
+mongoose.connect(mongoUri,options);
 
 var proxy = httpProxy.createProxyServer({});
 
